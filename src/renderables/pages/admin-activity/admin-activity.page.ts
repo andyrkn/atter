d977be48760aeba631b256b4e0a221099e630f8c
Cristian Andrei <cristyurs@yahoo.com>
2018-10-31 18:22:59 +0200

    general-info-table

--- src/renderables/pages/admin-activity/admin-activity.page.ts
+++ src/renderables/pages/admin-activity/admin-activity.page.ts
@@ -1,5 +1,4 @@
 import { Renderable } from "@web/core";
-import { Router } from "@web/router";
 
 @Renderable({
     folderPathRelativeToRenderablesFolder: 'pages/admin-activity',
@@ -9,6 +8,30 @@ import { Router } from "@web/router";
 export class AdminActivityPage {
 
     public id: number = 1;
-    constructor(private router: Router) {
+    public classInfo: ClassInfo = new ClassInfo();
+    constructor() {
+        console.log(this.classInfo.weekDays);
+    }
+}
+
+
+class ClassInfo {
+    public className: string;
+    public description: string;
+    public admin: string;
+    public meetingDays: any = [];
+    public startingTime: string;
+    public endingTime: string;
+    public status: string;
+    public weekDays: any = [];
+    constructor() {
+        this.admin = "Andrei";
+        this.description = " Description";
+        this.status = "Active";
+        this.className="Test class";
+        this.weekDays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+        this.meetingDays = ["Wed", "Fri","Sun"];
+        this.startingTime = "15:00";
+        this.endingTime = "16:00";
     }
 }
