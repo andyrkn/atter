bf1466c1f94a34c5f375b721b4d12502352b2b6a
Verciuc Sergiu <v_sergiu2009@yahoo.com>
2018-10-31 19:41:01 +0200

    Fix build errors

--- src/renderables/pages/admin-activity/admin-activity.page.ts
+++ src/renderables/pages/admin-activity/admin-activity.page.ts
@@ -1,4 +1,5 @@
 import { Renderable } from "@web/core";
+import { ClassInfo } from "./class-info";
 
 @Renderable({
     folderPathRelativeToRenderablesFolder: 'pages/admin-activity',
@@ -11,27 +12,5 @@ export class AdminActivityPage {
     public classInfo: ClassInfo = new ClassInfo();
     constructor() {
         console.log(this.classInfo.weekDays);
-    }
-}
-
-
-class ClassInfo {
-    public className: string;
-    public description: string;
-    public admin: string;
-    public meetingDays: any = [];
-    public startingTime: string;
-    public endingTime: string;
-    public status: string;
-    public weekDays: any = [];
-    constructor() {
-        this.admin = "Andrei";
-        this.description = " Description";
-        this.status = "Active";
-        this.className="Test class";
-        this.weekDays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
-        this.meetingDays = ["Wed", "Fri","Sun"];
-        this.startingTime = "15:00";
-        this.endingTime = "16:00";
     }
 }
---
+++ src/renderables/pages/admin-activity/class-info.ts
@@ -0,0 +1,20 @@
+export class ClassInfo {
+    public className: string;
+    public description: string;
+    public admin: string;
+    public meetingDays: any = [];
+    public startingTime: string;
+    public endingTime: string;
+    public status: string;
+    public weekDays: any = [];
+    constructor() {
+        this.admin = "Andrei";
+        this.description = " Description";
+        this.status = "Active";
+        this.className = "Test class";
+        this.weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+        this.meetingDays = ["Wed", "Fri", "Sun"];
+        this.startingTime = "15:00";
+        this.endingTime = "16:00";
+    }
+}
