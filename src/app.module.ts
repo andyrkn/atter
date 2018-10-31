45d8d75f9da543cae7ee2b992c0da65b6282be4c
Mihaila Andrei <andyfbgm@gmail.com>
2018-10-31 15:41:23 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@web/core";
 import { appRoute } from "./app.routing";
-import { HomePage, LoginPage, NotFoundPage, RegisterPage, ForgotPasswordPage, Dashboard, Activity, AdminActivityPage } from "./renderables/pages";
+import {
+    HomePage, LoginPage, NotFoundPage, RegisterPage,
+    ForgotPasswordPage, Dashboard, Activity, AdminActivityPage
+} from "./renderables/pages";
 import { HeaderComponent } from "./renderables/components";
 import { FillerDataService } from "./services/filler.data.service";
 import { UserService } from "./services/user.service";
@@ -18,7 +21,6 @@ import { UserService } from "./services/user.service";
             Activity,
             ForgotPasswordPage,
             AdminActivityPage
-
         ]
     },
     injectable: [
--- src/app.routing.ts
+++ src/app.routing.ts
@@ -1,6 +1,9 @@
 import { Route } from "@web/router";
 
-import { HomePage, LoginPage, NotFoundPage, LayoutPage, RegisterPage, ForgotPasswordPage, Dashboard, Activity, AdminActivityPage } from "./renderables/pages";
+import {
+    HomePage, LoginPage, NotFoundPage, LayoutPage,
+    RegisterPage, ForgotPasswordPage, Dashboard, Activity, AdminActivityPage
+} from "./renderables/pages";
 
 export const appRoute: Route[] = [
     {
@@ -35,7 +38,7 @@ export const appRoute: Route[] = [
         page: ForgotPasswordPage,
         path: 'forgot-password'
     },
-    {  
+    {
         page: AdminActivityPage,
         path: 'admin-activity'
 
