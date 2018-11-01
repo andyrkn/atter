79ad7395e89a38da859ceee26a4c27ac21b7d755
Mihaila Andrei <andyfbgm@gmail.com>
2018-11-02 01:31:48 +0200

    icons and hrefs

--- src/services/filler.data.service.ts
+++ src/services/filler.data.service.ts
@@ -11,36 +11,58 @@ export class FillerDataService {
         {
             name: 'arctic activity',
             id: 1,
-            coordinator: 'Cristi'
+            coordinator: 'Cristi',
+            iconId: 3
         }, {
             name: 'CLIW',
             id: 2,
-            coordinator: 'Sergiu'
+            coordinator: 'Sergiu',
+            iconId: 1
         }, {
             name: 'another activity',
             id: 3,
-            coordinator: 'Andrei'
+            coordinator: 'Andrei',
+            iconId: 5
         }, {
             name: 'cristi\'s activity',
             id: 4,
-            coordinator: 'Buraga'
+            coordinator: 'Buraga',
+            iconId: 4
         }
     ];
     private _myActivities: any = [
         {
-            name: 'laosdjhkfldasf'
+            name: 'laosdjhkfldasf',
+            id: 1,
+            iconId: 1
         }, {
-            name: 'CLIW!!!!'
+            name: 'CLIW!!!!',
+            id: 2,
+            iconId: 5
         }, {
-            name: 'anuheuheuhe>?y'
+            name: 'anuheuheuhe>?y',
+            id: 3,
+            iconId: 2
         }, {
-            name: 'cpadurety'
+            name: 'cpadurety',
+            id: 4,
+            iconId: 3
         }, {
-            name: 'arctos din cer'
+            name: 'arctos din cer',
+            id: 5,
+            iconId: 4
         }, {
-            name: 'steauta'
+            name: 'steauta',
+            id: 6,
+            iconId: 2
         }, {
-            name: 'cristi\'s activity'
+            name: 'cristi\'s activity',
+            id: 7,
+            iconId: 1
+        }, {
+            name: 'cristi\'s activity',
+            id: 8,
+            iconId: 6
         }
     ];
 
