ab4fcbcdcd1390ad982efe310c0210a6e3521422
Cristian Andrei <cristyurs@yahoo.com>
2019-01-20 14:48:45 +0200

    import data fixes and additions

--- src/renderables/pages/import-data-for-activity/import-data-for-activity.page.ts
+++ src/renderables/pages/import-data-for-activity/import-data-for-activity.page.ts
@@ -1,21 +1,25 @@
-import { Renderable } from "@web/core";
+import { Renderable, TrackChanges } from "@web/core";
 import { ExternalDataService } from "@app/services/external.data.service";
 import { DropboxImporter } from "@app/services/data-importer/dropbox.importer";
 import { UrlTree } from "@web/router";
-import { BehaviorSubject } from "rxjs";
 @Renderable({
     template: require('./import-data-for-activity.page.html'),
     style: require('./import-data-for-activity.page.css')
 })
 export class ImportDataForActivity {
     private activityID = new UrlTree().routeParameter;
-    public dropBoxFiles = [];
+    private acceptedExtensions = [".json", ".csv", ".xls"];
+    @TrackChanges()
+    public dropBoxFiles: [] = [];
 
     constructor(private externalDataService: ExternalDataService, private dropboxImporter: DropboxImporter) { }
 
     public getFilesFromDropBox() {
         this.externalDataService.obtainFiles(this.dropboxImporter).subscribe((data) => {
             const entries = JSON.parse(data.text).entries;
+            for (const key in entries) {
+            // REMOVE UNWANTED EXTENSIONS
+            }
             this.dropBoxFiles = entries;
         });
     }
